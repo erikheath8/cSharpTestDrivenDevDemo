56b9d394d24eedbdb623096b8a715eade35af17d
Erik Heath <erikheath8@gmail.com>
2021-06-12 17:24:24 -0400

    first & last commit, tdd cSharp demo followed from freecodecamp.org using stack methods

---
+++ Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack<T>
+    {
+        //region Members
+        private T[] stackArray;
+        private int maxLength;
+
+        //region Properties
+        public int Size { get; private set; }
+
+        //region constructor
+        public Stack(int length)
+        {
+            maxLength = length;
+            stackArray = new T[length];
+        }
+
+        public void Push(T value)
+        {
+            if (Size == maxLength)
+                throw new ExceededSizeException();
+
+            stackArray[Size++] = value;
+        }
+
+        public T Pop()
+        {
+            if (Size == 0)
+                throw new ExpenditureProhibitedException();
+
+            return stackArray[--Size];
+        }
+
+        public T Peek()
+        {
+            if (Size == 0)
+                throw new ExpenditureProhibitedException();
+
+            return stackArray[Size - 1];
+        }
+
+    }
+}
